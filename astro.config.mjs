02c0d2af4f38360aa04db10447caef0958ff63a8
Soyagvs <agustinmartinezyt@gmail.com>
2024-06-23 19:34:13 -0300

    add auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import auth from "auth-astro";
+import node from '@astrojs/node'; // Importa el adaptador de Node.js
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: 'server', // Configura la salida del proyecto para SSR
+  adapter: node({
+    mode: 'standalone' // Especifica el modo aquí
+  }),
+  integrations: [tailwind(), auth()],
 });
