62ecd9a4a79aff731d798840fb8c5de760248281
Soyagvs <agustinmartinezyt@gmail.com>
2024-07-08 23:59:24 -0300

    res

---
+++ src/components/FaqComponent.jsx
@@ -0,0 +1,337 @@
+export const FaqComponent = () => {
+  return (
+      <div
+          className="hs-accordion-group flex flex-col justify-center max-w-[800px] mx-4 mt-20"
+      >
+          <div
+              className="hs-accordion hs-accordion-active:border-gray-200 border border-transparent rounded-xl bg-white"
+              id="hs-active-bordered-heading-one"
+          >
+              <button
+                  className="text-2xl hs-accordion-toggle inline-flex justify-between items-center gap-x-3 w-full font-semibold text-start py-4 px-5 hover:text-[#80ED99] disabled:opacity-50 disabled:pointer-events-none text-[#7F56D9]"
+                  aria-controls="hs-basic-active-bordered-collapse-one"
+              >
+                  what is the objective of CONEX ?
+                  <svg
+                      className="hs-accordion-active:hidden block size-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                  >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5v14"></path>
+                  </svg>
+                  <svg
+                      className="hs-accordion-active:block hidden size-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                  >
+                      <path d="M5 12h14"></path>
+                  </svg>
+              </button>
+              <div
+                  id="hs-basic-active-bordered-collapse-one"
+                  className="hs-accordion-content hidden overflow-hidden transition-[height] duration-300"
+                  aria-labelledby="hs-active-bordered-heading-one"
+              >
+                  <div className="pb-4 px-5">
+                      <p className="text-[#667085]">
+                          The objective of CONEX is to form the platform where
+                          the most outstanding web developers on the planet
+                          are.
+                      </p>
+                  </div>
+              </div>
+          </div>
+
+          <div
+              className="hs-accordion hs-accordion-active:border-gray-200 active bg-white border border-transparent rounded-xl"
+              id="hs-active-bordered-heading-two"
+          >
+              <button
+                  className="text-2xl hs-accordion-toggle inline-flex justify-between items-center gap-x-3 w-full font-semibold text-start py-4 px-5 hover:text-[#80ED99] disabled:opacity-50 disabled:pointer-events-none text-[#7F56D9]"
+                  aria-controls="hs-basic-active-bordered-collapse-two"
+              >
+                  who is the founder of CONEX
+                  <svg
+                      className="hs-accordion-active:hidden block size-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                  >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5v14"></path>
+                  </svg>
+                  <svg
+                      className="hs-accordion-active:block hidden size-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                  >
+                      <path d="M5 12h14"></path>
+                  </svg>
+              </button>
+              <div
+                  id="hs-basic-active-bordered-collapse-two"
+                  className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
+                  aria-labelledby="hs-active-bordered-heading-two"
+              >
+                  <div className="pb-4 px-5">
+                      <p className="text-[#667085]">
+                          The founder of CONEX is a young developer who did
+                          this self-taught and ended up creating one of the
+                          most outstanding platforms in the working world of
+                          programmers.
+                      </p>
+                  </div>
+              </div>
+          </div>
+
+          <div
+              className="hs-accordion hs-accordion-active:border-gray-200 bg-white border border-transparent rounded-xl"
+              id="hs-active-bordered-heading-three"
+          >
+              <button
+                  className="text-2xl hs-accordion-toggle inline-flex justify-between items-center gap-x-3 w-full font-semibold text-start py-4 px-5 hover:text-[#80ED99] disabled:opacity-50 disabled:pointer-events-none text-[#7F56D9]"
+                  aria-controls="hs-basic-active-bordered-collapse-three"
+              >
+                  Can I work for CONEX ?
+                  <svg
+                      className="hs-accordion-active:hidden block size-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                  >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5v14"></path>
+                  </svg>
+                  <svg
+                      className="hs-accordion-active:block hidden size-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                  >
+                      <path d="M5 12h14"></path>
+                  </svg>
+              </button>
+              <div
+                  id="hs-basic-active-bordered-collapse-three"
+                  className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+                  aria-labelledby="hs-active-bordered-heading-three"
+              >
+                  <div className="pb-4 px-5">
+                      <p className="text-[#667085]">
+                          Yes, CONEX is constantly renewing its employees for
+                          the professional development of the website using
+                          the best tools on the market.
+                      </p>
+                  </div>
+              </div>
+          </div>
+
+          <div
+              className="hs-accordion hs-accordion-active:border-gray-200 bg-white border border-transparent rounded-xl"
+              id="hs-active-bordered-heading-three"
+          >
+              <button
+                  className="text-2xl hs-accordion-toggle inline-flex justify-between items-center gap-x-3 w-full font-semibold text-start py-4 px-5 hover:text-[#80ED99] disabled:opacity-50 disabled:pointer-events-none text-[#7F56D9]"
+                  aria-controls="hs-basic-active-bordered-collapse-three"
+              >
+                  How can I register and create an account ?
+                  <svg
+                      className="hs-accordion-active:hidden block size-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                  >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5v14"></path>
+                  </svg>
+                  <svg
+                      className="hs-accordion-active:block hidden size-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                  >
+                      <path d="M5 12h14"></path>
+                  </svg>
+              </button>
+              <div
+                  id="hs-basic-active-bordered-collapse-three"
+                  className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+                  aria-labelledby="hs-active-bordered-heading-three"
+              >
+                  <div className="pb-4 px-5">
+                      <p className="text-[#667085]">
+                          To register, click the "Register" button at the top
+                          right of the page. Complete the form with your
+                          personal information and follow the instructions to
+                          verify your account. Once verified, you will be able
+                          to log in and access all the site's features.
+                      </p>
+                  </div>
+              </div>
+          </div>
+          <div
+              className="hs-accordion hs-accordion-active:border-gray-200 bg-white border border-transparent rounded-xl"
+              id="hs-active-bordered-heading-three"
+          >
+              <button
+                  className="text-2xl hs-accordion-toggle inline-flex justify-between items-center gap-x-3 w-full font-semibold text-start py-4 px-5 hover:text-[#80ED99] disabled:opacity-50 disabled:pointer-events-none text-[#7F56D9]"
+                  aria-controls="hs-basic-active-bordered-collapse-three"
+              >
+                  How can I update my account information ?
+                  <svg
+                      className="hs-accordion-active:hidden block size-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                  >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5v14"></path>
+                  </svg>
+                  <svg
+                      className="hs-accordion-active:block hidden size-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                  >
+                      <path d="M5 12h14"></path>
+                  </svg>
+              </button>
+              <div
+                  id="hs-basic-active-bordered-collapse-three"
+                  className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+                  aria-labelledby="hs-active-bordered-heading-three"
+              >
+                  <div className="pb-4 px-5">
+                      <p className="text-[#667085]">
+                          To update your account information, log in and go to
+                          the "Settings" section. There you can modify your
+                          email address and password. Be sure to save your
+                          changes before leaving the page.
+                      </p>
+                  </div>
+              </div>
+          </div>
+          <div
+              className="hs-accordion hs-accordion-active:border-gray-200 bg-white border border-transparent rounded-xl"
+              id="hs-active-bordered-heading-three"
+          >
+              <button
+                  className="text-2xl hs-accordion-toggle inline-flex justify-between items-center gap-x-3 w-full font-semibold text-start py-4 px-5 hover:text-[#80ED99] disabled:opacity-50 disabled:pointer-events-none text-[#7F56D9]"
+                  aria-controls="hs-basic-active-bordered-collapse-three"
+              >
+                  How does the matching process between programmers and
+                  recruiters work ?
+                  <svg
+                      className="hs-accordion-active:hidden block size-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                  >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5v14"></path>
+                  </svg>
+                  <svg
+                      className="hs-accordion-active:block hidden size-3.5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                  >
+                      <path d="M5 12h14"></path>
+                  </svg>
+              </button>
+              <div
+                  id="hs-basic-active-bordered-collapse-three"
+                  className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+                  aria-labelledby="hs-active-bordered-heading-three"
+              >
+                  <div className="pb-4 px-5">
+                      <p className="text-[#667085]">
+                          Our matching algorithm takes into account several
+                          factors, including skills, experience, and work
+                          preferences of programmers, as well as the
+                          requirements and culture of recruiting companies.
+                      </p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  )
+}
+
+export default FaqComponent 
